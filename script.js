5611290a46541ee7fc06249ef99d09771ca9cf4b
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-07-27 13:10:56 +0530

    script.js

--- script.js
+++ script.js
@@ -275,7 +275,7 @@ function createExperienceSection() {
     const group = new THREE.Group();
     
     resumeData.experience.forEach((exp, index) => {
-        const cardGeometry = new THREE.BoxGeometry(30, 14, 1.5);
+        const cardGeometry = new THREE.BoxGeometry(50, 14, 1.5);
         const cardTexture = createTextTexture(`🏢 ${exp.company}\n\n👨‍💻 ${exp.position}\n📅 ${exp.duration}\n\n${exp.description}`, 40, '#e74c3c');
         const cardMaterial = new THREE.MeshLambertMaterial({ 
             map: cardTexture,
